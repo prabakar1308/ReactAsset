bd0b14d55638e6581cc4e4b9a5b1c8e0a9776a25
prabakar1308 <prabakar1308@gmail.com>
2016-08-25 02:20:44 +0530

    Add table structure to team details page using react bootstrap table

--- test/src/js/components/TeamDetails.jsx
+++ test/src/js/components/TeamDetails.jsx
@@ -1,31 +1,72 @@
 import React from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+
+const employees = [];
+
+function addProducts(quantity) {
+  const startId = employees.length;
+  for (let i = 0; i < quantity; i++) {
+    const id = startId + i;
+    employees.push({
+      id: id,
+      name: 'Name ' + id,
+      track: 'MS Track',
+      level: 'C1',
+      role: 'Developer',
+      asset: 'Yes',
+      ip: '10.174.128.9',
+    });
+  }
+}
+
+var addEmployee = function(){
+    employees.push({
+      id: 100,
+      name: 'Praba',
+      track: 'MS Track',
+      level: 'C1',
+      role: 'Developer',
+      asset: 'Yes',
+      ip: '10.174.128.9',
+    });
+}
+
+addProducts(3);
+var selectRowProp = {
+  mode: "radio", // or checkbox
+  clickToSelect: true,
+    bgColor: "rgb(135, 206, 250)",
+  onSelect: onRowSelect
+};
+
+function onRowSelect(row, isSelected){
+  console.log(row);
+  console.log("selected: " + isSelected)
+}
+
+/*var tableOptions = {
+    afterInsertRow: this.onAfterInsertRow.bind(this),
+    afterDeleteRow: this.onAfterDeleteRow.bind(this)
+}*/
+
+
 export default class TeamDetails extends React.Component {
 
   render() {
     return (
-      <div>
-        <h3>Team Information</h3>
-        <p>List of active team members:</p>
-        <p>Delivery Manager - <b>Mahesh T</b></p>
-        <p>Project Manager - <b>Jacqulin</b></p>
-        <dl>
-        <dt>Java Track</dt>
-            <dd>- Ramesh</dd>
-            <dd>- Mahesh</dd>
-            <dd>- Shyjith</dd>
-        <dt>MS Track</dt>
-            <dd>- Vasanth</dd>
-            <dd>- Bhoopathy</dd>
-            <dd>- Uma</dd>
-        <dt>QA Track</dt>
-            <dd>- Venkat</dd>
-            <dd>- Vijay</dd>
-            <dd>- Abinash</dd>
-        <dt>DB Track</dt>
-            <dd>- Karthik</dd>
-            <dd>- Nithya</dd>
-        </dl>
-      </div> 
+        <div>
+        <p></p>
+      <BootstrapTable data={ employees } insertRow={addEmployee} deleteRow={true} selectRow={selectRowProp} search={true}
+        striped={true} hover={true}  height='120' width='150' /*columnFilter={true}  pagination={true} */>
+          <TableHeaderColumn width='140' dataField='id' isKey={ true }>Employee Id</TableHeaderColumn>
+          <TableHeaderColumn width='140' dataField='name'  dataSort={true}>Name</TableHeaderColumn>
+          <TableHeaderColumn width='140' dataField='track'>Track</TableHeaderColumn>
+        <TableHeaderColumn width='140' dataField='level'>Competency</TableHeaderColumn>
+        <TableHeaderColumn width='140' dataField='role'>Role</TableHeaderColumn>
+        <TableHeaderColumn width='140' dataField='asset'>Asset</TableHeaderColumn>
+        <TableHeaderColumn width='140' dataField='ip'>Static IP</TableHeaderColumn>
+      </BootstrapTable>
+        </div>
     );
   }
 
